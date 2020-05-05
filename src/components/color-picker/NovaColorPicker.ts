6aeb8b018ec67da0c229b8f860f1151bfcdc0f54
em2046 <em2046@qq.com>
2020-05-05 14:09:17 +0800

    color-picker

--- src/components/color-picker/NovaColorPicker.ts
+++ src/components/color-picker/NovaColorPicker.ts
@@ -22,37 +22,47 @@ export default defineComponent({
           ]),
           h('div', { class: 'nova-color-picker-form' }, [
             h('div', { class: 'nova-color-picker-labels' }, [
-              h('div', { class: 'nova-color-picker-label-primary' }, 'R'),
-              h('div', { class: 'nova-color-picker-label-secondary' }, 'G'),
-              h('div', { class: 'nova-color-picker-label-tertiary' }, 'B'),
-              h('div', { class: 'nova-color-picker-label-quaternary' }, 'A%'),
-            ]),
-            h('div', { class: 'nova-color-picker-numbers' }, [
-              h(
-                'div',
-                { class: 'nova-color-picker-number-primary' },
-                h('input')
-              ),
-              h(
-                'div',
-                { class: 'nova-color-picker-number-secondary' },
-                h('input')
-              ),
-              h(
-                'div',
-                { class: 'nova-color-picker-number-tertiary' },
-                h('input')
-              ),
-              h(
-                'div',
-                { class: 'nova-color-picker-number-quaternary' },
-                h('input')
-              ),
+              h('label', { class: 'nova-color-picker-label-primary' }, [
+                h('div', { class: 'nova-color-picker-label-text' }, 'R'),
+                h(
+                  'div',
+                  { class: 'nova-color-picker-number-primary' },
+                  h('input', { value: 127 })
+                ),
+              ]),
+              h('label', { class: 'nova-color-picker-label-secondary' }, [
+                h('div', { class: 'nova-color-picker-label-text' }, 'G'),
+                h(
+                  'div',
+                  { class: 'nova-color-picker-number-secondary' },
+                  h('input', { value: 63 })
+                ),
+              ]),
+              h('label', { class: 'nova-color-picker-label-tertiary' }, [
+                h('div', { class: 'nova-color-picker-label-text' }, 'B'),
+                h(
+                  'div',
+                  { class: 'nova-color-picker-number-tertiary' },
+                  h('input', { value: 63 })
+                ),
+              ]),
+              h('label', { class: 'nova-color-picker-label-quaternary' }, [
+                h('div', { class: 'nova-color-picker-label-text' }, 'A%'),
+                h(
+                  'div',
+                  { class: 'nova-color-picker-number-quaternary' },
+                  h('input', { value: 100 })
+                ),
+              ]),
             ]),
             h(
               'div',
               { class: 'nova-color-picker-rgb' },
-              h('div', { class: 'nova-color-picker-hex' }, h('input'))
+              h(
+                'div',
+                { class: 'nova-color-picker-hex' },
+                h('input', { value: '#7f3f3f' })
+              )
             ),
           ]),
           h('div', { class: 'nova-color-picker-preview' }, [
